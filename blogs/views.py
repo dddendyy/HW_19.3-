5575876b4e6b1b633ce3272d8086bc480165476b
dddendyy <dbokasin@yandex.ru>
2024-07-03 22:38:08 +0300

    Реализовано: 1. перенаправление на DetailView в UpdateView через get_success_url() 2. динамическое формирование slug'a через form_valid() 3. фильтр по опубликованным блогам через et_queryset()

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from pytils.translit import slugify
 
 from blogs.models import Blog
 
@@ -12,17 +13,40 @@ class BlogListView(ListView):
     model = Blog
     template_name = 'blogs/blog_list.html'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(is_published=True)
+
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'date', 'is_published')
     success_url = reverse_lazy('blogs:list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'date', 'is_published')
-    success_url = reverse_lazy('blogs:list')
+    # success_url = reverse_lazy('blogs:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blogs:detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDetailView(DetailView):
