1ace7d74f08579e856044f13384ceff78f8c6de5
dddendyy <dbokasin@yandex.ru>
2024-07-12 01:50:21 +0300

    Реализованы clean-методы для форм продуктов

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,9 +2,29 @@ from django import forms
 
 from catalog.models import Product
 
+forbidden_phrases = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get('name')
+
+        for phrase in forbidden_phrases:
+            if phrase in cleaned_data:
+                raise forms.ValidationError('Запрещенное выражение в названии!')
+
+        return cleaned_data
+
+    def clean_desc(self):
+        cleaned_data = self.cleaned_data.get('desc')
+
+        for phrase in forbidden_phrases:
+            if phrase in cleaned_data:
+                raise forms.ValidationError('Запрещенное выражение в описании!')
+
+        return cleaned_data
