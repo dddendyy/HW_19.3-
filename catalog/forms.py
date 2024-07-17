86dbd7eab814a45d94e3523fff27285c68b99174
dddendyy <dbokasin@yandex.ru>
2024-07-17 23:56:28 +0300

    Добавлена стилизация форм и формсетов

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,11 +1,22 @@
 from django import forms
+from django.forms import BooleanField
 
 from catalog.models import Product, Version
 
 forbidden_phrases = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
 
     class Meta:
         model = Product
@@ -30,7 +41,7 @@ class ProductForm(forms.ModelForm):
         return cleaned_data
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = '__all__'
