2f7531acc270e4bc9e05aecb567e3c10ecc9cea8
dddendyy <dbokasin@yandex.ru>
2024-07-28 00:48:19 +0300

    Переопределён метод form_valid() для отправки письма после регистрации

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
@@ -11,11 +13,19 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_form = form.save()
+            sended_message = f'Аккаунт с почтой {new_form.email} успешно зарегистрирован'
+            send_mail('Оповещение о регистрации', sended_message, EMAIL_HOST_USER, [str(new_form.email)])
+
+        return super().form_valid(form)
+
 
 class ProfileView(UpdateView):
     model = User
     form_class = UserProfileForm
     success_url = reverse_lazy('users:profile')
 
-    def get_object(self, queryser=None):
+    def get_object(self, queryset=None):
         return self.request.user
