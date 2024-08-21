f8fb96e0e31269b56a8d8549c7e5d85ee025e8d7
dddendyy <dbokasin@yandex.ru>
2024-08-22 00:54:24 +0300

    Реализовано низкоуровневое кеширование списка категорий

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.forms import inlineformset_factory
 from django.urls import reverse_lazy
@@ -6,6 +7,8 @@ from django.views.generic import TemplateView, ListView, DetailView, CreateView,
 
 from catalog.forms import ProductForm, VersionForm, ProductModeratorForm
 from catalog.models import Product, Version, Category
+from catalog.service import get_categories_from_cache
+from config import settings
 
 
 class ContactTemplateView(TemplateView):
@@ -23,17 +26,8 @@ class CategoryListView(LoginRequiredMixin, ListView):
 class ProductsListView(LoginRequiredMixin, ListView):
     model = Product
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        products = self.get_queryset()
-        versions = {}
-        for product in products:
-            current_version = Version.objects.filter(product_id=product.pk, is_current=True)
-            if current_version:
-                current_version = Version.objects.get(product_id=product.pk, is_current=True)
-                versions[product.pk] = current_version
-        context_data['current_version'] = versions
-        return context_data
+    def get_queryset(self):
+        return get_categories_from_cache()
 
 
 class ProductCardDetailView(LoginRequiredMixin, DetailView):
